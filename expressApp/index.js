558581fe1b4dba02f216bb0f32e974b8b4aa37f1
Poreniyi <poreniyi@gmail.com>
2020-08-21 13:52:03 -0400

    added views,assetfolder  and get itemsRoute

--- expressApp/index.js
+++ expressApp/index.js
@@ -2,15 +2,24 @@ const express=require('express');
 const app=express();
 const port=3000;
 const path=require('path');
+let assetPath=path.join(__dirname, './assets');
+console.log(assetPath);
+app.use('/assets',express.static(assetPath));
 // const fetch=require('';)
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','html');
+app.set('views', path.join(__dirname,'./views'));
+app.set('view engine','ejs');
+
+
+
 app.get('/',(req,res)=>{
     console.log(`Home page visited`);
-    res.send();
+    res.render("MarketFu");
 })
-app.get('/ot',(req,res)=>{
-
+app.get('/getItems',async (req,res)=>{
+    const getItemData=require('./fetch.js').getItemData;
+    let items=await getItemData(1);
+    console.log('getItems triggered',items);
+    res.send(items);
 })
 
 app.listen(port,()=>{
