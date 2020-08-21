0237f4b6c387ccafb1473b767ed859d60500737c
Poreniyi <poreniyi@gmail.com>
2020-08-20 23:28:46 -0400

    made assets folder

---
+++ expressApp/assets/Scripts/MarketFu.js
@@ -0,0 +1,10 @@
+let table=document.getElementById('itemTable');
+console.log(table);
+
+makeRequest=async()=>{
+    let response= await fetch();
+}
+
+makeRequest2=async()=>{
+    let response= await fetch();
+}
